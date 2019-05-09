c005762c876250046a5dd9c2450004edb23dc354
Jordan Richards <jordyrichards@gmail.com>
2019-05-09 23:58:17 +0100

    add picture to hompage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -6,9 +6,8 @@ class HomePage extends Component {
     render() {
         return(
             <div className="HomePage">
-        
-            <h1 class="mainTitle">Jordan and Alex's Wedding!</h1> 
-            <p class="countdown-label">Days &nbsp;&nbsp;&nbsp; Hours &nbsp;&nbsp; Mins &nbsp;&nbsp; Secs</p>
+            <img className="polaroid" src={require('./Polaroid_collage.png')} alt="photo collage" width="50%" height="50%" />
+            <p class="countdown-label">Days &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Hours &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Mins &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Secs</p>
                 <div className="countdown">
                     <Countdown date={new Date('2019-11-02T13:00:00')} />
                 </div>
